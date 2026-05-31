e767b4c0fbcb3ea7f0a8229bf0b61b34cf30c64b
Scott Fones <scott.fones@gmail.com>
2026-05-30 21:01:14 -0400

    chore: remove unused configs for theme, sccache, python venv

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -2,19 +2,12 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# set -gx RUSTC_WRAPPER sccache
-
-# theme
-# source ~/.config/fish/themes/kanagawa.fish
-
-# Ruby user gem binaries (e.g. neovim-ruby-host, bundler if installed)
+# Ruby user gem binaries
 for gem_bin in $HOME/.local/share/gem/ruby/*/bin
     test -d $gem_bin; and fish_add_path --prepend --path $gem_bin
 end
 
-# nvm, npm, node
+# Tool inits
 bass source ~/.nvm/nvm.sh
-
-# source /home/scott/.python_general_venv/bin/activate.fish
 starship init fish | source
 zoxide init fish | source
