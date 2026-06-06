82273babf4fa5f8170d1851acf1f697097e33a73
Scott Fones <scott.fones@gmail.com>
2026-06-06 13:46:38 -0400

    feat: added CommitMono and AtkynsonMono NF to wezterm. added MonaspiceNe entry to wezterm with all features enabled. updated wezterm media glyph to nu

--- private_dot_config/wezterm/fonts.lua
+++ private_dot_config/wezterm/fonts.lua
@@ -11,8 +11,30 @@ local module = {}
 -- function that accepts the config object, like this:
 function module.apply_to_config(config)
 	config.font = wezterm.font_with_fallback({
+		{ family = "CommitMono Nerd Font Propo", weight = "Regular" },
+		{ family = "AtkynsonMono Nerd Font Propo", weight = "Medium" },
 		{ family = "FiraCode Nerd Font Propo", weight = "Medium" },
 		{ family = "MonaspiceNe Nerd Font Propo", weight = "Medium" },
+		{
+			family = "MonaspiceNe Nerd Font Propo",
+			weight = "Medium",
+			harfbuzz_features = {
+				"kern",
+				"liga",
+				"clig",
+				"calt",
+				"ss01",
+				"ss02",
+				"ss03",
+				"ss04",
+				"ss05",
+				"ss06",
+				"ss07",
+				"ss08",
+				"ss09",
+				"ss10",
+			},
+		},
 		{ family = "MonaspiceKr Nerd Font Propo", weight = "Medium" },
 		{ family = "MonaspiceAr Nerd Font Propo", weight = "Medium" },
 		{ family = "MonaspiceXe Nerd Font Propo", weight = "Medium" },
