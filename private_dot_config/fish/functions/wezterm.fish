e1115bdd68a553192a4dca8a930744283db75b1f
Scott Fones <scott.fones@gmail.com>
2026-06-05 21:40:40 -0400

    feat: added fish shell alias for locally compiled wezterm

---
+++ private_dot_config/fish/functions/wezterm.fish
@@ -0,0 +1,3 @@
+function wezterm --description 'user compiled wezterm'
+    /home/scott/Applications/Wezterm/wezterm/target/release/wezterm --config-file /home/scott/.config/wezterm/wezterm.lua $argv
+end
