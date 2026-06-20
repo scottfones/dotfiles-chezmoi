00eaeccbce94c7eb270636473d1825114392318b
Scott Fones <scott.fones@gmail.com>
2026-06-20 18:40:22 -0400

    fix: bash and fish import users systemd env file variables with systemd built-in and path dedup
    fix: bashrc no longer a template

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -2,6 +2,19 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# Reset systemd environment variables
+if not set -q __SHELL_ENV_IMPORTED
+    if test -x /usr/lib/systemd/user-environment-generators/30-systemd-environment-d-generator
+        bass 'while IFS= read -r l; do [ -n "$l" ] && eval "export $l"; done < <(/usr/lib/systemd/user-environment-generators/30-systemd-environment-d-generator)'
+        set -l seen
+        for p in $PATH
+            contains -- $p $seen; or set -a seen $p
+        end
+        set -gx PATH $seen
+        set -gx __SHELL_ENV_IMPORTED 1
+    end
+end
+
 # Ruby user gem binaries
 for gem_bin in $HOME/.local/share/gem/ruby/*/bin
     test -d $gem_bin; and fish_add_path --prepend --path $gem_bin
