f77be2692cf260dba22214b70b5948cb4033f12b
Scott Fones <scott.fones@gmail.com>
2026-08-24 16:12:18 -0400

    fix(bash, fish): add xauthority env var to export. fixes tmux boot/restore env var copy gap

--- private_dot_config/fish/conf.d/tmux_client_env.fish
+++ private_dot_config/fish/conf.d/tmux_client_env.fish
@@ -3,7 +3,7 @@ function __tmux_sync_client_env --on-event fish_prompt
     test -n "$TMUX"; or return
 
     set -l tmux_env (tmux show-environment 2>/dev/null)
-    for var in SSH_CONNECTION DISPLAY WAYLAND_DISPLAY
+    for var in SSH_CONNECTION DISPLAY WAYLAND_DISPLAY XAUTHORITY
         set -l entry (string match -r -- "^$var=.*" $tmux_env)
         if test -n "$entry"
             set -gx $var (string replace -r -- "^$var=" '' $entry)
